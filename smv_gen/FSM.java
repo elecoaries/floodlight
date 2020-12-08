55c4bc0274f732f6b467c9bee1c6987264bfb683
William Li <william.li10@icloud.com>
2020-12-08 10:06:47 -0500

    tested smv generation and added FSM.java to smv_gen

---
+++ smv_gen/FSM.java
@@ -0,0 +1,60 @@
+package net.floodlightcontroller.packethandler;
+
+import net.floodlightcontroller.packet.IPv4;
+
+enum State {
+	INITIAL {
+		@Override
+		State event(String eventName, boolean isTrue) {
+			if (eventName == "infected") {
+				if (isTrue)
+					return INFECTED;
+			}
+			return INITIAL;
+		}
+	},
+
+	INFECTED {
+		@Override
+		State event(String eventName, boolean isTrue) {
+			if (eventName == "infected") {
+				if (!isTrue)
+					return INITIAL;
+			}
+			return INFECTED;
+		}
+	};
+
+	abstract State event(String eventName, boolean eventValue);
+}
+
+public class FSM extends AFSM {
+
+	public State state;
+
+	public FSM() {
+		state = State.INITIAL;
+	}
+
+	@Override
+	public int eventHandler(String eventName, boolean eventValue) {
+		state = state.event(eventName, eventValue);
+		if (state == State.INITIAL) {
+			return IDENTIFY;
+		} else if (state == State.INFECTED) {
+			return DROP;
+		}
+		return NULL;
+	}
+
+	@Override
+	public int getAction() {
+		if (state == State.INITIAL) {
+			return IDENTIFY;
+		} else if (state == State.INFECTED) {
+			return DROP;
+		}
+		return NULL;
+	}
+
+}
--- smv_gen/SMV_Generator.java
+++ smv_gen/SMV_Generator.java
@@ -1,5 +1,3 @@
-package net.floodlightcontroller.packethandler;
-
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
